6ff465b5c548a916ee2281cce17abf1bb6a95754
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-05-01 08:42:05 -0300

    Create tests for checking column, row and square

--- index.js
+++ index.js
@@ -7,6 +7,44 @@ module.exports = parseBoard = (board) =>
         );
 
 //saveEmptyPosition
-//checkRow checkColumn checkSquare checkValue
+
+module.exports.saveEmptyPositions = (board) => {
+    let emptyPositions = [];
+    board.map((row, i) => 
+        row.map((column, j) => 
+            column == 0 ? 
+            emptyPositions.push([i, j]) : 
+            column));
+    return emptyPositions;
+}
+//checkRow 
+module.exports.checkRow = (board, y, num) => {
+    for(let i = 0; i < board[y].length; i++){
+        if(board[y][i] == num) return false;
+    }
+    return true;
+}
+//checkColumn 
+module.exports.checkColumn = (board, x, num) => {
+    for(let i = 0; i < board[x].length; i++){
+        if(board[i][x] == num) return false;
+    }
+    return true;
+}
+//checkSquare 
+module.exports.check3x3Square = (board, y, x, num) => {
+
+    let yStart = Math.floor( y / 3 ) * 3;
+    let xStart = Math.floor( x / 3 ) * 3;
+
+    for(let i = yStart; i < yStart + 2; i++){
+        for(let j = xStart; j < xStart + 2; j++){
+            if(board[i][j] == num) return false;
+        }
+    }
+
+    return true;
+}
+//checkValue
 //solvePuzzle
 //solveSudoku
--- sudoku_solver_spec.js
+++ sudoku_solver_spec.js
@@ -38,5 +38,53 @@ describe('Sudoku Solver', function(){
               expect(parsedBoard[0].length).to.equal(9);
               expect(parsedBoard).to.eql(expectedBoard);
         })
+
+        describe('#saveEmptyPositions()', function () {
+            it('should save all of the empty positions, 0s, in a parsed board', function () {
+                emptyPositions = solver.saveEmptyPositions(parsedBoard);
+
+                let expectedPositions = [
+                    [0,0],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[1,0],[1,1],
+                    [1,2],[1,5],[2,0],[2,1],[2,2],[2,6],[2,7],[2,8],[3,0],
+                    [3,1],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[4,4],[4,5],
+                    [4,7],[4,8],[5,1],[5,3],[5,4],[5,5],[5,8],[6,1],[6,2],
+                    [6,4],[6,6],[6,8],[7,1],[7,3],[7,4],[7,5],[7,8],[8,1],
+                    [8,2],[8,3],[8,5],[8,6],[8,7],[8,8]
+                ];
+
+                expect(emptyPositions.length).to.equal(51);
+                expect(emptyPositions).to.eql(expectedPositions);
+            })
+        })
+
+
+        describe('#checkRow()', function () {
+            it('should check that each value in the row does not equal the input', function () {
+                //Not input
+                expect(solver.checkRow(parsedBoard, 0, 2)).to.be.ok;
+                //Eql input
+                expect(solver.checkRow(parsedBoard, 0, 9)).to.not.be.ok;
+            })
+        })
+
+        describe('#checkColumn()', function() {
+            it('should check that each value in a column does not equal the input', function() {
+              // Not input
+              expect(solver.checkColumn(parsedBoard, 0, 9)).to.be.ok;
+              // Eql input
+              expect(solver.checkColumn(parsedBoard, 0, 5)).to.not.be.ok;
+            });
+        })
+
+        describe('#checkSquare()', function () {
+            it('should check that each value in a 3x3 square does not match the input', function () {
+                //No match (true)
+                expect(solver.check3x3Square(parsedBoard, 2, 2, 1)).to.be.ok;
+                expect(solver.check3x3Square(parsedBoard, 7, 7, 9)).to.be.ok;
+                //Match (false)
+                expect(solver.check3x3Square(parsedBoard, 2, 2, 9)).to.not.be.ok;
+                expect(solver.check3x3Square(parsedBoard, 7, 7, 1)).to.not.be.ok;
+            })
+        })
     })
 })
